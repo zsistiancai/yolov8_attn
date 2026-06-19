7363e431241a5b01d927ef319e72ab4e16d6de9b
UltralyticsAssistant <web@ultralytics.com>
2026-06-19 04:13:39 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/attnmodules/ECA.py
+++ ultralytics/nn/attnmodules/ECA.py
@@ -1,26 +1,30 @@
-import torch
-import torch.nn as nn
+from __future__ import annotations
+
 import math
 
+import torch.nn as nn
+
+
 class ECA(nn.Module):
-    def __init__(self, channels:int, k_size:int=None, gamma:int=2, b:int=1):
-        super(ECA, self).__init__()
+    def __init__(self, channels: int, k_size: int | None = None, gamma: int = 2, b: int = 1):
+        super().__init__()
         assert channels > 0
         self.gap = nn.AdaptiveAvgPool2d(1)
         if k_size is None:
             t = int(abs(math.log2(channels) / gamma + b))
-            k_size = t if t % 2 else t+1
+            k_size = t if t % 2 else t + 1
             k_size = max(3, k_size)
-        self.conv1d = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size-1)//2, bias=False)
+        self.conv1d = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        y = self.gap(x)                             # (B, C, 1, 1)
-        y = y.squeeze(-1).squeeze(-1)               # (B, C)
-        y = y.unsqueeze(1)                          # (B, 1, C)
-        y = self.conv1d(y)                          # (B, 1, C)
-        y = self.sigmoid(y).squeeze(1).unsqueeze(-1).unsqueeze(-1) # (B, C, 1, 1)
+        y = self.gap(x)  # (B, C, 1, 1)
+        y = y.squeeze(-1).squeeze(-1)  # (B, C)
+        y = y.unsqueeze(1)  # (B, 1, C)
+        y = self.conv1d(y)  # (B, 1, C)
+        y = self.sigmoid(y).squeeze(1).unsqueeze(-1).unsqueeze(-1)  # (B, C, 1, 1)
         return x * y
+
 
 # if __name__ == '__main__':
 #     x = torch.randn(25, 1024, 13, 13)
